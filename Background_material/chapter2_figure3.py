3bcd6597ab619ba48aeb897656685a126bf89643
StevenElsworth <steven.elsworth@sky.com>
2020-03-31 08:43:23 +0100

    add ARMA example

--- Background_material/chapter2_figure3.py
+++ Background_material/chapter2_figure3.py
@@ -1,1 +1,17 @@
 import matplotlib.pyplot as plt
+from pandas import read_csv
+import numpy as np
+from statsmodels.tsa.arima_model import ARIMA
+
+series = read_csv('shampoo_sales.csv', header=0, index_col=0)
+X = series.values
+size = int(len(X) * 0.8)
+train, test = X[0:size], X[size:len(X)]
+model = ARIMA(train, order=(3,1,3))
+fitted_model = model.fit(disp=0)
+
+prediction = fitted_model.forecast(len(X)-size)
+plt.plot(train, 'k')
+plt.plot(np.arange(size-1, size+len(test)), [train[-1]]+test.tolist())
+plt.plot(np.arange(size-1, size+len(test)), [train[-1]]+prediction[0].tolist(), color='red')
+plt.show()
